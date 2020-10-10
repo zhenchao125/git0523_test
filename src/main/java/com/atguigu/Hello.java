e2d015605df3626e057384f941b5dc0832af29e4
zhangsan <zhangsan@atguigu.com>
2020-10-10 11:17:05 +0800

    update....

---
+++ src/main/java/com/atguigu/Hello.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @Author lzc
+ * @Date 2020/10/10 11:15
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
